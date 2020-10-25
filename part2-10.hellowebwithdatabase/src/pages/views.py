80ef57649ce4448641ccd5b32d946a354a5a82cb
doguhanyeke <doguyeke@gmail.com>
2020-10-25 23:14:08 +0300

    part2-10.hellowebwithdatabase

--- part2-10.hellowebwithdatabase/src/pages/views.py
+++ part2-10.hellowebwithdatabase/src/pages/views.py
@@ -5,7 +5,7 @@ from .models import Message
 # Create your views here.
 
 def homePageView(request):
-	content = 'Hello Web!';
-
-		
-	return HttpResponse(content)
+	idM = request.GET.get("id")
+	print(idM)
+	mes = Message.objects.get(id=idM);
+	return HttpResponse(mes.content)
